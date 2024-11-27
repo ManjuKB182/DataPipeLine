21406e45c183d2e279a0e92d5e3566ee19abd205
ManjuKB182 <contactkbmanju@gmail.com>
2024-11-27 13:07:02 +0530

    transformationfilecommit

---
+++ tranformation.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("Test Spark Installation") \
+    .getOrCreate()
+
+#print("Spark session created successfully!")
+
+file_path="/Users/manjukb/Desktop/ETL_DataBase/File01.csv"
+
+df = spark.read.csv(file_path, header=True, inferSchema=True)
+df.show(4)
+
+df.createOrReplaceTempView("BaseTable")
+
+True_value_table = spark.sql('''
+                             Select *
+                             From BaseTable
+                             Where Cryosleep = "true"   
+                                ''')
+file_Out_path = "/Users/manjukb/Desktop/ETL_DataBase/truevalues.csv"
+True_value_table.coalesce(2).write.csv(file_Out_path, header= True, mode= "overwrite")
+
+spark.stop()
