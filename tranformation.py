3f1f002acff53ec8f024527d3831eb539eb9d531
ManjuKB182 <contactkbmanju@gmail.com>
2025-01-17 19:52:39 +0530

    Newchanges1701

--- tranformation.py
+++ tranformation.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 # Create a Spark session
 spark = SparkSession.builder \
     .appName("Test Spark Installation") \
+    .config('spark.sql.warehouse.dir', 'file:///C:/') \
     .getOrCreate()
 
 #print("Spark session created successfully!")
@@ -23,3 +24,49 @@ file_Out_path = "/Users/manjukb/Desktop/Work_Related/ETL_python_Automation/DataP
 True_value_table.coalesce(2).write.csv(file_Out_path, header= True, mode= "overwrite")
 
 spark.stop()
+
+
+
+
+
+spark = SparkSession.builder.appName('S3')/
+        .config('spark.hadoop.fs.aws.secrete.key:', 'org.apache.hadoop:hadoop-aws:2.7.4')/
+        .getOrCreate()
+
+
+# Read the data from S3
+s3_df = spark.read.csv('s3a://bucket_name/file.csv', header=True, inferSchema=True)
+
+s3_df.createOrReplaceTempView('s3_table')
+
+
+RS_jdbc_url = "jdbc:sqlserver://<server>:<port>;databaseName=<database>"
+RS_table = "dbo.table_name"
+RS_properties = {
+    "user": "<username
+    "password": "<password>"
+    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+}
+
+# Read the data from SQL Server
+RS_df = spark.read.jdbc(url=RS_jdbc_url, table=RS_table, properties=RS_properties)
+
+RS_df.createOrReplaceTempView('RS_table')
+
+#Check the data count
+
+s3_count = spark.sql('''Select count(*) 
+                     from s3_table''').collect()[0][0]
+
+RS_count = spark.sql('''Select count(*) 
+                     from RS_table''').collect()[0][0]
+
+if s3_count == RS_count:
+    print("Count is matching")
+else:
+    print("Count is not matching")
+
+RS_df.coalesce(1).write.csv('s3a://bucket_name/RS_table.csv', header=True, mode='overwrite')
+
+spark.stop()
+
